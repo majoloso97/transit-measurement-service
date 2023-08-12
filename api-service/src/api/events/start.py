d8ecc7a263ca8ccc7a5a171bbd13e40566736580
majoloso97 <majoloso97@gmail.com>
2023-08-12 13:36:34 -0600

    Test redis client start on fast api server start

--- api-service/src/api/events/start.py
+++ api-service/src/api/events/start.py
@@ -1,12 +1,14 @@
 import logging
 from typing import Callable
 from fastapi import FastAPI
+from redis import Redis
 from database.models import printing
 
 
 def start_handler(app: FastAPI) -> Callable:
     '''Things to do while starting the API server'''
     async def start_app() -> None:
-        logging.warning(printing())
+        r = Redis(password='admin')
+        logging.info(r.ping())
 
     return start_app
