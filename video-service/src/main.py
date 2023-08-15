d7b5458ea2d8f137c8564f6017cbfc1d3e3cf499
majoloso97 <majoloso97@gmail.com>
2023-08-14 18:37:39 -0600

    Remove fast api from video service

--- video-service/src/main.py
+++ video-service/src/main.py
@@ -1,23 +1,17 @@
-import uvicorn
-from fastapi import FastAPI
-from settings import settings
-from api.events.start import start_handler
-from api.routes.v1 import welcome, image
+import time
+import logging
+# from core.checks import get_ultralytics_checks
 
 
-app = FastAPI(title="Service for video processing in transit measurment app",
-              description="This is a REST API for interacting \
-                with the video processing service. \
-                It uses FastAPI as the web framework and YOLO \
-                model for ML operations.")
+logging.root.setLevel(logging.INFO)
 
-app.add_event_handler('startup', start_handler(app))
 
-app.include_router(welcome.router)
-app.include_router(image.router)
+def run_video_service():
+    # get_ultralytics_checks()
+    while True:
+        logging.info('Running Video Service')
+        time.sleep(60)
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",
-                host="0.0.0.0",
-                port=80,
-                reload=settings.AUTO_RELOAD)
+    run_video_service()
