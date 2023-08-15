a6c7a917ca5cce77ff92decfbae879b90dde55bf
majoloso97 <majoloso97@gmail.com>
2023-08-14 21:50:40 -0600

    Implement basic start of attached services in api

--- api-service/src/api/events/start.py
+++ api-service/src/api/events/start.py
@@ -1,15 +1,22 @@
 import logging
 from typing import Callable
 from fastapi import FastAPI
-from redis import Redis
-from shared.database.models import printing
+from shared.database.db import db
+from shared.database.models import Video
+from shared.queue.queue import q
+
+
+logger = logging.getLogger(__name__)
 
 
 def start_handler(app: FastAPI) -> Callable:
     '''Things to do while starting the API server'''
     async def start_app() -> None:
-        r = Redis(host='transit_measurement_service_queue', password='admin')
-        logging.info(r.ping())
-        logging.info(printing())
+        if not db.is_active:
+            raise ConnectionError('Database not connected')
+        if not q.ping():
+            raise ConnectionError('Database not connected')
+        logger.info('Postgres database connected')
+        logger.info('Redis queue connected')
 
     return start_app
