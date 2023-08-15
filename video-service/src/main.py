fc6abc0b0a5d55ae482d5b74719eb0ef172152a7
majoloso97 <majoloso97@gmail.com>
2023-08-14 21:51:16 -0600

    Implement basic start of attached services in video service

--- video-service/src/main.py
+++ video-service/src/main.py
@@ -1,13 +1,23 @@
 import time
 import logging
+from settings import settings
 from core.checks import get_ultralytics_checks
+from shared.database.db import db
+from shared.queue.queue import q
 
 
-logging.root.setLevel(logging.INFO)
+logging.root.setLevel(logging.getLevelName(settings.LOG_LEVEL))
+logger = logging.getLogger(__name__)
 
 
 def run_video_service():
     get_ultralytics_checks()
+    if not db.is_active:
+        raise ConnectionError('Database not connected')
+    if not q.ping():
+        raise ConnectionError('Database not connected')
+    logger.info('Postgres database connected')
+    logger.info('Redis queue connected')
     while True:
         logging.info('Running Video Service')
         time.sleep(60)
