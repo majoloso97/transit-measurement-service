a7df22868cd423484ef9bfcff59a95a380bd1fc5
majoloso97 <majoloso97@gmail.com>
2023-08-14 21:24:23 -0600

    Implement simple db service and video model

---
+++ shared/database/db.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import sessionmaker
+from shared.database.models import Base
+from settings import settings
+
+
+class DbService:
+    def __init__(self) -> None:
+        # try:
+            self.engine = create_engine(self.get_url())
+            Base.metadata.create_all(self.engine)
+            self.session = sessionmaker(self.engine)
+            self.is_active = True
+        # except Exception:
+        #     self.is_active = False
+
+    def get_url(self):
+        url = URL.create(drivername='postgresql',
+                         username=settings.POSTGRES_USER,
+                         password=settings.POSTGRES_PASSWORD,
+                         host=settings.POSTGRES_HOST,
+                         port=settings.POSTGRES_PORT,
+                         database=settings.POSTGRES_DB)
+        return url
+
+    def save(self, record):
+        if not self.is_active:
+            raise ConnectionError('DB not connected')
+        with self.session.begin() as session:
+            session.add(record)
+            session.commit()
+
+
+db = DbService()
--- shared/database/models.py
+++ shared/database/models.py
@@ -1,2 +1,17 @@
-def printing():
-    return 'Yes, the DB is set up'
+from sqlalchemy import (Column, text, func,
+                        String, DateTime)
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+
+
+class Video(Base):
+    __tablename__ = "videos"
+    id = Column(UUID,
+                server_default=text("gen_random_uuid()"),
+                primary_key=True)
+    uploaded_at = Column(DateTime(timezone=True),
+                         server_default=text("(now() at time zone 'utc')"))
+    name = Column(String(150))
