4727c70ebef8649c981439a96bcac87cf82ce37a
majoloso97 <majoloso97@gmail.com>
2023-08-14 19:54:35 -0600

    Centralize settings

--- shared/shared_settings.py
+++ shared/shared_settings.py
@@ -2,5 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class SharedSettings(BaseSettings):
+    LOG_LEVEL: str = 'INFO'
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
     REDIS_HOST: str
+    REDIS_PORT: int
     REDIS_PASSWORD: str
+    AWS_REGION: str
+    AWS_KEY_ID: str
+    AWS_SECRET: str
