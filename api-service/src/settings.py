1861d34342e8d8ee84402cb7bfe08d2c9b0bd9d3
majoloso97 <majoloso97@gmail.com>
2023-08-14 19:55:07 -0600

    Remove centralized settings

--- api-service/src/settings.py
+++ api-service/src/settings.py
@@ -3,7 +3,6 @@ from shared.shared_settings import SharedSettings
 
 class Settings(SharedSettings):
     FAST_API_KEY: str
-    LOG_LEVEL: str = 'INFO'
     AUTO_RELOAD: bool = False
 
 
--- video-service/src/settings.py
+++ video-service/src/settings.py
@@ -3,9 +3,6 @@ from shared.shared_settings import SharedSettings
 
 class Settings(SharedSettings):
     MODEL_NAME: str
-    FAST_API_KEY: str
-    LOG_LEVEL: str = 'INFO'
-    AUTO_RELOAD: bool = False
 
 
 settings = Settings() 
