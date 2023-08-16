749458411cc97fc931071a9cdfa7106840f77dcc
majoloso97 <majoloso97@gmail.com>
2023-08-16 13:58:30 -0600

    Remove commented code

--- video-service/src/core/checks.py
+++ video-service/src/core/checks.py
@@ -1,8 +1,5 @@
 from ultralytics import checks
-# from ultralytics.utils import LOGGER
-# from log_config import setup_logger
 
 
 def get_ultralytics_checks():
-    # setup_logger(LOGGER)
     return checks()
