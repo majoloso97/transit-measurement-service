afc2dcc8b0a1b3b363c63e7b6b5eb1ee30928f3c
majoloso97 <majoloso97@gmail.com>
2023-08-20 18:52:21 -0600

    Add schemas for users and auth

---
+++ shared/schemas/users.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import EmailStr, BaseModel, ConfigDict
+
+
+class BaseUser(BaseModel):
+    id: Optional[int] = None
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    password: str
+    email: Optional[EmailStr] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    description: Optional[str] = None
+    photo: Optional[str] = None
+    created_at: datetime
+    last_active: datetime
+    is_active: bool
+
+
+class NewUser(BaseUser):
+    username: str
+    password: str
+    created_at: datetime = datetime.utcnow()
+    last_active: datetime = datetime.utcnow()
+    is_active: bool = True
+
+
+class ModifiedUser(BaseUser):
+    password: Optional[str] = None
+    email: Optional[EmailStr] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    description: Optional[str] = None
+    photo: Optional[str] = None
+    is_active: Optional[bool] = None
+    last_active: Optional[datetime] = None
