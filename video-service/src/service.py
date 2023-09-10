62689682c81633af71099141e4eaccdee3cef050
majoloso97 <majoloso97@gmail.com>
2023-09-09 23:16:49 -0600

    Test read video from presigned url

--- video-service/src/service.py
+++ video-service/src/service.py
@@ -6,6 +6,9 @@ from core.optimizer import VideoOptimizer
 from core.processor import VideoProcessor
 from shared.database.db import db
 from shared.queue.queue import q
+from shared.aws.factory import AWSServiceFactory
+import cv2
+from supervision.utils.video import VideoInfo, VideoSink
 
 
 def run_video_service(logger: logging.Logger):
@@ -16,10 +19,21 @@ def run_video_service(logger: logging.Logger):
     logger.info('Postgres database connected')
     logger.info('Redis queue connected')
     get_ultralytics_checks()
-    # optimizer = VideoOptimizer(video_id=1)
-    # optimizer.optimize()
-    # vid = VideoProcessor(video_id=2)
-    # vid.predict()
+    s3 = AWSServiceFactory.get_service(service='s3',
+                                       config= {'bucket_name': 'transit-ventures-test'})
+    url_opt = s3.generate_presigned_url('my_video_opt.mp4')
+    url = s3.get_presigned_url('my_video.mp4')
+    
+    info = VideoInfo.from_video_path(url)
+    logging.warning(f'INFO: {info}')
+    
+    vidcap = cv2.VideoCapture(url)
+    with VideoSink('src/core/assets/test.mp4', info) as sink:
+        for _ in range(100):
+            success, frame = vidcap.read()
+            if not success: break
+            sink.write_frame(frame)
+
     while True:
         logger.info('Running Video Service')
         try:
