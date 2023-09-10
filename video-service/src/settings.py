cec149078f0b4a8d73f2f08d2ef264831d07fe1d
majoloso97 <majoloso97@gmail.com>
2023-09-10 12:50:33 -0600

    Add optimization parameters to video service settings

--- video-service/src/settings.py
+++ video-service/src/settings.py
@@ -3,6 +3,8 @@ from shared.shared_settings import SharedSettings
 
 class Settings(SharedSettings):
     MODEL_NAME: str = 'yolov8n.pt'
+    MAX_FPS: int = 15
+    MAX_BASE_DIMENSION: int = 360
 
 
 settings = Settings() 
