e64733712fab7eaed0ad1c86ce24e9258a195400
majoloso97 <majoloso97@gmail.com>
2023-09-10 23:52:03 -0600

    WIP comment out optimizer calls

--- video-service/src/service.py
+++ video-service/src/service.py
@@ -2,13 +2,10 @@ import sys
 import time
 import logging
 from core.checks import get_ultralytics_checks
-from core.optimizer import VideoOptimizer
-from core.processor import VideoProcessor
+# from core.optimizer import VideoOptimizer
+# from core.processor import VideoProcessor
 from shared.database.db import db
 from shared.queue.queue import q
-from shared.aws.factory import AWSServiceFactory
-import cv2
-from supervision.utils.video import VideoInfo, VideoSink
 
 
 def run_video_service(logger: logging.Logger):
@@ -19,21 +16,10 @@ def run_video_service(logger: logging.Logger):
     logger.info('Postgres database connected')
     logger.info('Redis queue connected')
     get_ultralytics_checks()
-    s3 = AWSServiceFactory.get_service(service='s3',
-                                       config= {'bucket_name': 'transit-ventures-test'})
-    url_opt = s3.generate_presigned_url('my_video_opt.mp4')
-    url = s3.get_presigned_url('my_video.mp4')
+    # vid = VideoOptimizer(8)
+    # # vid.test()
+    # vid.optimize()
     
-    info = VideoInfo.from_video_path(url)
-    logging.warning(f'INFO: {info}')
-    
-    vidcap = cv2.VideoCapture(url)
-    with VideoSink('src/core/assets/test.mp4', info) as sink:
-        for _ in range(100):
-            success, frame = vidcap.read()
-            if not success: break
-            sink.write_frame(frame)
-
     while True:
         logger.info('Running Video Service')
         try:
