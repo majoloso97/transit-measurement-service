7fbec5724cd990462449651834114fe236c6525a
majoloso97 <majoloso97@gmail.com>
2023-11-02 09:35:08 +0100

    Remove duplicate logic

--- shared/service/videos.py
+++ shared/service/videos.py
@@ -63,14 +63,7 @@ class VideoManager:
         measurement.video_id = video_id
         measurement.output_s3_key = self.generate_video_key('predictions')
         get_coord = lambda m: [m.x1, m.y1, m.x2, m.y2]
-        request_coordinates = get_coord(measurement)
-        if not all(request_coordinates):
-            measurement.x1 = 0
-            measurement.y1 = 0.5
-            measurement.x2 = 1
-            measurement.y2 = 0.5
-
-        coordinate_cond = [all([isinstance(c, int), 0<=c<=1])
+        coordinate_cond = [all([isinstance(c, float), 0<=c<=1])
                            for c in get_coord(measurement)]
         if not all(coordinate_cond):
             measurement.x1 = 0
